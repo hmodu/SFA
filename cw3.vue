696bdd65bc441855c753d08fa6250ec98a75b989
Modu <35815941+hmodu@users.noreply.github.com>
2021-07-07 17:07:44 +0400

    Add files via upload

---
+++ cw3.vue
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>CW 2 - Individual</title>
+    <script src="https://unpkg.com/vue"> </script>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import Materialize CSS Stylesheet-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
+    <!-- Font Awesome icon -->
+    <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    rel="stylesheet">
+
+   
+</head>
+
+<html>
+<template>
+    <StackLayout>
+        <Label class="h1 text-center" text="Checkout" style="color:white" />
+        <Label class="h5 text-center" text="tap lesson to remove it cart"
+            style="color:green" />
+        <ListView for="product in cart" @itemTap="onItemTap"
+            style="color:white; height:50%">
+            <v-template>
+                <StackLayout>
+                    <Label :text="product.subject" />
+                    <Label :text="`price: ${product.price}`" />
+                    <Label
+                        :text="`inventory: ${product.availableInventory}`" />
+                </StackLayout>
+            </v-template>
+        </ListView>
+        <TextField style="color:white;" hint=" name"
+            v-model="checkout.name" />
+        <TextField style="color:black;" hint=" phone number"
+            v-model="checkout.phone" />
+        <Button @tap="submit" text="Submit Order" />
+    </StackLayout>
+</template>
+
+<script>
+    export default {
+        props: {
+            cart: {
+                type: Array
+            },
+            products: {
+                type: Array
+            }
+        },
+        data() {
+            return {
+                checkout: {
+                    name: "",
+                    phone: ""
+                }
+            };
+        },
+
+        methods: {
+            onItemTap(event) {
+                this.$emit("removeProduct", event.item);
+            },
+            submitForm() {
+                fetch("https://moscst3145.herokuapp.com/collection/orders", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        Name: this.order.name,
+                        Phone: this.order.phone,
+                        ProductsInOrders: this.cart.map(({
+                            subject
+                        }) => subject)
+                    })
+                }).then(function() {});
+                this.cart.forEach(element => {
+                    fetch(
+                        "https://moscst3145.herokuapp.com/collection/products/" +
+                        element._id, {
+                            method: "PUT",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                availableInventory: element
+                                    .availableInventory
+                            })
+                        }
+                    ).then(function(data) {});
+                });
+                alert("Order submitted");
+            },
+            cartCount(id) {
+                let count = 0;
+                for (let i = 0; i < this.cart.length; i++) {
+                    if (this.cartID[i] === id) {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    };
+</script>
+<style>
+</style>
+</body>
+
+</html>
---
+++ productsList.vue
@@ -0,0 +1,37 @@
+<template>
+    <ListView for="product in products" @itemTap="onItemTap"
+        style="color:white; height:100%">
+        <v-template>
+            <DockLayout height="500" stretchLastChild="false">
+                <Image dock="top" height="200"
+                    :src="`https://hmodu.github.io/Course-work-2/Client/${product.image}`"
+                    height="50%" width="100%" />
+                <Label
+                    :text="`Available Inventory: ${product.availableInventory}`"
+                    dock="bottom" width="100%" />
+                <Label :text="`price: $${product.price}`" dock="bottom"
+                    width="100%" />
+                <Label :text="`location: ${product.location}`" dock="bottom"
+                    width="100%" />
+                <Label :text="product.subject" dock="bottom" />
+            </DockLayout>
+        </v-template>
+    </ListView>
+</template>
+
+<script>
+    export default {
+        props: ["products"],
+        data() {
+            return {};
+        },
+        methods: {
+            onItemTap(event) {
+                this.$emit("addProduct", event.item);
+            }
+        }
+    };
+</script>
+
+<style>
+</style>
